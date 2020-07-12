152accd97aef4dc0bc8d32abcf3a4165c5ae8c5b
BitLoopLV <66575644+BitLoopLV@users.noreply.github.com>
2020-07-12 10:50:17 -0700

    Update AmazonPriceChecker.java

--- src/amazonpricechecker/AmazonPriceChecker.java
+++ src/amazonpricechecker/AmazonPriceChecker.java
@@ -6,23 +6,17 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class AmazonPriceChecker {
-	
-	static String amazonItemUrl = "https://www.amazon.com/Texas-Instruments-Graphing-Calculator-Frustration/dp/B07SPVP7D4/";
 
 	public static void main(String[] args) {
+		String amazonItemUrl = "https://www.amazon.com/Apple-MacBook-1-8GHz-dual-core-Intel/dp/B07211W6X2/ref=sr_1_4?dchild=1&keywords=macbook&qid=1594575939&s=electronics&sr=1-4";
 		try {
 			Document amazonPage = Jsoup.connect(amazonItemUrl).get();
-			String price = amazonPage.getElementById("priceblock_ourprice").text();
-			System.out.println("The price of this item is: "+price);
-			double priceNum = Double.parseDouble(price.replace("$", ""));
-			if(priceNum < 170) {
-				System.out.println("Wow that's a great deal");
-			}
+			String priceStr = amazonPage.getElementById("priceblock_ourprice").text();
+			System.out.println(priceStr);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
 }
